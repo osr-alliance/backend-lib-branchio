cbe3ec9b9e2549ec1b94fbb6581abdc114ab8b0e
lushas <lucasaa@gmail.com>
2022-05-27 17:39:45 -0300

    Created method to buildParameters

--- structs.go
+++ structs.go
@@ -1,7 +1,9 @@
 package branchio
 
+import "encoding/json"
+
 type DeepLinkProperties struct {
-	Channel  string                 `json:"channel"`
+	Channel  string                 `json:"channel,omitempty"`
 	Feature  string                 `json:"feature,omitempty"`
 	Stage    string                 `json:"stage,omitempty"`
 	Campaign string                 `json:"campaign,omitempty"`
@@ -28,4 +30,30 @@ func (b *branchio) mergeMaps(maps ...map[string]interface{}) map[string]interfac
 		}
 	}
 	return mergedMaps
+}
+
+func (b *branchio) structToMap(data interface{}) (map[string]interface{}, error) {
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	mapData := make(map[string]interface{})
+	err = json.Unmarshal(dataBytes, &mapData)
+	return mapData, nil
+}
+
+func (b *branchio) buildParameters(deepLinkProperties *DeepLinkProperties, data *DeepLinkData, customData map[string]interface{}) (map[string]interface{}, error) {
+	dataMap, err := b.structToMap(data)
+	if err != nil {
+		return nil, err
+	}
+
+	customDataMap := b.mergeMaps(dataMap, customData)
+	if deepLinkProperties == nil {
+		return customDataMap, nil
+	}
+
+	deepLinkProperties.Data = customDataMap
+
+	return b.structToMap(deepLinkProperties)
 }
